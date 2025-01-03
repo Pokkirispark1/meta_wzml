1ee107c7d54e2b2192e4c245bf45b4987883464d
Pokkirispark1 <125215338+Pokkirispark1@users.noreply.github.com>
2025-01-03 16:26:33 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,10 +2,10 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = '🦉Owner'
-    ST_BN1_URL = 'https://t.me/aboutRizzx'
-    ST_BN2_NAME = '🌨️'
-    ST_BN2_URL = 'https://t.me/+XUQg9HjxnS5iYzY1'
+    ST_BN1_NAME = 'Owner👑'
+    ST_BN1_URL = 'https://t.me/SPA4KBRO'
+    ST_BN2_NAME = 'CHANNEL'
+    ST_BN2_URL = 'https://t.me/MOVIEMANIA_TG'
     ST_MSG = '''<blockquote><b>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.Type {help_command} to get a list of available commands</b></blockquote>'''
     ST_BOTPM = '''<blockquote><i>Now, This bot will send all your files and links here. Start Using ...</blockquote></i>'''
     ST_UNAUTH = '''<blockquote><i>You Are not authorized user!</i></blockquote>'''
